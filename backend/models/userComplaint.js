a8c3277ffbcb4088a06e2eb4eff7563190388c6e
T-j-A-n <156585811+T-j-A-n@users.noreply.github.com>
2025-04-26 12:59:08 +0530

    Update userComplaint.js

--- backend/models/userComplaint.js
+++ backend/models/userComplaint.js
@@ -1,38 +1,16 @@
 const mongoose = require('mongoose');
-const userAuthentication = new mongoose.Schema({
-    UserName:{
-        type: String,
-        required: true,
-    },
-    Password: {
-        type: String,
-        required: true
-    }
 
-})
-const userSchema = new mongoose.Schema({
-    UserName:{
-        type: String,
-        required: true,
-    },
-    ListOfComplains:{
-        type:[Number],
-        default: 0
-    }
-})
 const userComplaintSchema = new mongoose.Schema({
   //id: {type: Number, required: true},
   userName: { type: String, required: true },
   title: { type: String, required: true },
   description: { type: String, required: false },
   category: { type: String, required: true },
-  image: { type: String, required: true },
+  image: { type: String, required: false },
   location: { type: String, required: true },
+  upvotes:{typee: String, default: 0}
   //priority: { type: Number, required: true },
 });
 
 const User = mongoose.model('User', userComplaintSchema);
-const Complaint = mongoose.model('userComplaintSchema', userComplaintSchema)
-const Auth = mongoose.model('userAuthentication', userAuthentication)
-
-module.exports = {User, Complaint, Auth};
+module.exports = User;
